f5fd7caffecaabb0806fc51e6366f3d369321707
Pierrick Turelier <pierrick@turelier.com>
2021-09-27 19:41:00 -0500

    chore(lint): add prettier config and lint files

--- hardhat.config.polygon.js
+++ hardhat.config.polygon.js
@@ -1,7 +1,7 @@
-const config = require('./hardhat.config')
+const config = require('./hardhat.config');
 
 config.etherscan = {
-  apiKey: process.env.POLYGONSCAN_API_KEY
-}
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+};
 
-module.exports = config
+module.exports = config;

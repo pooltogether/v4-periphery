9fa45135f20d7271b0ffc0901717dd1a7d7e8235
kames <kamescg@gmail.com>
2022-03-03 14:21:30 -0700

    update(PrizeTierHistoryV2): use virtual functions to resolve drawId

--- contracts/PrizeTierHistoryV2.sol
+++ contracts/PrizeTierHistoryV2.sol
@@ -24,34 +24,13 @@ contract PrizeTierHistoryV2 is DrawBinarySearch {
 
     PrizeTier[] internal history;
 
-    function getPrizeTier(uint32 _drawId) external view returns (PrizeTier memory) {
-        require(_drawId > 0, "PrizeTierHistory/draw-id-not-zero");
-        return _getPrizeTier(_drawId);
+    function getNewestIndex() internal view override returns (uint32) {
+        return uint32(history.length - 1);
     }
 
-    function _getPrizeTier(uint32 _drawId) internal view returns (PrizeTier memory) {
-        return history[0];
+    function getDrawIdForIndex(uint index) internal view override returns (uint32) {
+        return history[index].drawId;
     }
-}
-
-/**
- * @title  PoolTogether V4 PrizeTierHistoryUsingLibV2
- * @author PoolTogether Inc Team
- * @notice PrizeTierHistoryV2 manages a history of PrizeTier parameters
- */
- contract PrizeTierHistoryUsingLibV2 {
-    struct PrizeTier {
-        uint8 bitRangeSize;
-        uint32 drawId;
-        uint32 maxPicksPerUser;
-        uint32 expiryDuration;
-        uint32 endTimestampOffset;
-        uint256 prize;
-        uint32[16] tiers;
-        uint32 dpr;
-    }
-
-    PrizeTier[] internal history;
 
     function getPrizeTier(uint32 _drawId) external view returns (PrizeTier memory) {
         require(_drawId > 0, "PrizeTierHistory/draw-id-not-zero");
@@ -59,25 +38,6 @@ contract PrizeTierHistoryV2 is DrawBinarySearch {
     }
 
     function _getPrizeTier(uint32 _drawId) internal view returns (PrizeTier memory) {
-        uint32 cardinality = uint32(history.length);
-        require(cardinality > 0, "PrizeTierHistory/no-prize-tiers");
-
-        uint32 leftSide = 0;
-        uint32 rightSide = cardinality - 1;
-        uint32 oldestDrawId = history[leftSide].drawId;
-        uint32 newestDrawId = history[rightSide].drawId;
-
-        require(_drawId >= oldestDrawId, "PrizeTierHistory/draw-id-out-of-range");
-        if (_drawId >= newestDrawId) return history[rightSide];
-        if (_drawId == oldestDrawId) return history[leftSide];
-
-        uint32[] memory _history = new uint32[](cardinality);
-        
-        for (uint256 index = 0; index < _history.length; index++) {
-            _history[index] = history[index].drawId;
-        }
-
-        uint32 i = BinarySearchLib._binarySearch(_drawId, leftSide, rightSide, _history);
-        return history[i];
+        return history[_binarySearch(_drawId)];
     }
 }

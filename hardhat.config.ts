2e0f953d226e373f46b55ada7bfd8b851022e40c
Pierrick Turelier <pierrick@turelier.com>
2022-10-24 15:31:11 -0500

    chore(package): bump packages and add typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
+import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
 import 'hardhat-abi-exporter';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
@@ -91,6 +93,10 @@ const config: HardhatUserConfig | any = {
                 },
             },
         ],
+    },
+    typechain: {
+        outDir: './types',
+        target: 'ethers-v5',
     },
 };
 

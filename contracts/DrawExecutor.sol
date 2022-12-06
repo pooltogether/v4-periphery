848d14fdb355c7ca4fbc9430aca6648ee4671828
Pierrick Turelier <pierrick@turelier.com>
2022-12-05 21:24:05 -0600

    feat(contracts): add DrawExecutor

---
+++ contracts/DrawExecutor.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.6;
+
+import "@pooltogether/v4-core/contracts/interfaces/IDrawBuffer.sol";
+
+import { ExecutorAware } from "./abstract/ExecutorAware.sol";
+
+contract DrawExecutor is ExecutorAware {
+    address public immutable drawRelayer;
+
+    IDrawBuffer public immutable drawBuffer;
+
+    constructor(
+        address _executor,
+        address _drawRelayer,
+        IDrawBuffer _drawBuffer
+    ) ExecutorAware(_executor) {
+        require(address(_drawRelayer) != address(0), "DE/drawRelayer-not-zero-address");
+        require(address(_drawBuffer) != address(0), "DE/drawBuffer-not-zero-address");
+
+        drawRelayer = _drawRelayer;
+        drawBuffer = _drawBuffer;
+    }
+
+    function pushDraw(IDrawBeacon.Draw calldata _draw) external {
+        _checkSender();
+
+        drawBuffer.pushDraw(_draw);
+    }
+
+    function pushDraws(IDrawBeacon.Draw[] calldata _draws) external {
+        _checkSender();
+
+        uint256 _drawsLength = _draws.length;
+
+        for (uint256 i; i < _drawsLength; i++) {
+            drawBuffer.pushDraw(_draws[i]);
+        }
+    }
+
+    function _checkSender() internal view {
+      require(_msgSender() == address(drawRelayer), "DE/l1-sender-not-relayer");
+      require(isTrustedExecutor(msg.sender), "DE/l2-sender-not-executor");
+    }
+}
